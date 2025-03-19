58bb58d682c7977cfa93ec51bd4aa6f3422d72a2
Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
2025-03-19 14:56:23 +0200

    KOHA-1501: Add info where plugin is used
    This patch adds info about where plugin is used
    to the plugin description.

--- Koha/Plugin/Fi/KohaSuomi/ChangesToSearchPage.pm
+++ Koha/Plugin/Fi/KohaSuomi/ChangesToSearchPage.pm
@@ -22,7 +22,7 @@ our $metadata = {
     minimum_version => '23.11',
     maximum_version => '',
     version         => $VERSION,
-    description     => "Muutokset tiedonhakuun (otsikot eri kielillä aineistotyyppi-, hyllytarkenne- ja ikärajavälilehdille tiedonhaussa. Lisätään alasvetovalikoihin YKL, UDK, Päivittyvä julkaisu ja Kausijulkaisu)",
+    description     => "Muutokset tiedonhakuun. Otsikot eri kielillä aineistotyyppi-, hyllytarkenne- ja ikärajavälilehdille tiedonhaussa. Lisätään alasvetovalikoihin YKL, UDK, Päivittyvä julkaisu ja Kausijulkaisu. (Paikalliskannat, Täti)",
 };
 
 ## This is the minimum code required for a plugin's 'new' method
